9ddec865cd011cd4f268cccf4e8557272693739c
Aki <akshithtata6@gmail.com>
2026-08-14 22:16:02 +1200

    Creating PrivacyPage

--- TestingApplication/Views/PrivacyPage.xaml.cs
+++ TestingApplication/Views/PrivacyPage.xaml.cs
@@ -1,9 +1,28 @@
-namespace TestingApplication.Views;
-
-public partial class PrivacyPage : ContentPage
+namespace VolunteerConnect2.Views
 {
-	public PrivacyPage()
-	{
-		InitializeComponent();
-	}
+    public partial class PrivacyPage : ContentPage
+    {
+        public PrivacyPage()
+        {
+            InitializeComponent();
+            SetupActions();
+        }
+
+        private void SetupActions()
+        {
+            ClearMessagesButton.Clicked += async (s, e) =>
+            {
+                bool confirm = await DisplayAlert(
+                    "Clear Messages",
+                    "This will clear any temporary messages shown in the app. Your registrations will not be deleted.",
+                    "Clear",
+                    "Cancel");
+
+                if (confirm)
+                {
+                    await DisplayAlert("Messages Cleared", "Temporary messages have been cleared.", "OK");
+                }
+            };
+        }
+    }
 }
