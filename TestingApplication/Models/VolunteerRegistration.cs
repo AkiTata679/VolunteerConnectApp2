c2891585f391602ae1924ea3f2aeee11015a9a09
Aki <akshithtata6@gmail.com>
2026-08-14 21:58:27 +1200

    Creating VolunteerRegistration

--- TestingApplication/Models/VolunteerRegistration.cs
+++ TestingApplication/Models/VolunteerRegistration.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestingApplication.Models
+﻿namespace VolunteerConnect2.Models
 {
-    internal class VolunteerRegistration
+    public class VolunteerRegistration
     {
+        public int Id { get; set; }
+        public int OpportunityId { get; set; }
+
+        public string PreferredName { get; set; }
+        public string ContactDetail { get; set; }
+        public string Availability { get; set; }
+        public string Notes { get; set; }
+
+        public bool ConsentGiven { get; set; }
+        public DateTime RegistrationDate { get; set; }
     }
 }
