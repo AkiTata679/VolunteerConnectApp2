d04ddb928e25cfd263a6dd3d01d99edb679967c0
Aki <akshithtata6@gmail.com>
2026-08-14 22:05:20 +1200

    Creating RegistrationService

--- TestingApplication/Services/RegistrationService.cs
+++ TestingApplication/Services/RegistrationService.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VolunteerConnect2.Models;
+using SQLite;
 
-namespace TestingApplication.Services
+namespace VolunteerConnect2.Services
 {
-    internal class RegistrationService
+    public class RegistrationService
     {
+        private SQLiteAsyncConnection _db => DatabaseService.GetConnection();
+
+        public async Task<List<VolunteerRegistration>> GetAllAsync()
+        {
+            return await _db.Table<VolunteerRegistration>().ToListAsync();
+        }
+
+        public async Task<VolunteerRegistration> GetByIdAsync(int id)
+        {
+            return await _db.Table<VolunteerRegistration>()
+                .Where(r => r.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task AddAsync(VolunteerRegistration registration)
+        {
+            await _db.InsertAsync(registration);
+        }
+
+        public async Task UpdateAsync(VolunteerRegistration registration)
+        {
+            await _db.UpdateAsync(registration);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var reg = await GetByIdAsync(id);
+            if (reg != null)
+                await _db.DeleteAsync(reg);
+        }
     }
 }
